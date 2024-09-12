50001b1291a85b62acb9cb3f2a36638871a9a2e0
palstr <palstrgames@gmail.com>
2024-09-12 10:51:16 +0200

    kolekcja

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -21,6 +24,33 @@ public class Main {
         for (int element:tablicaLiczbLosowych) {
             System.out.print(element + ", ");
         }
+
+        System.out.println(" ");
+
+        //wypelnianie kolekcji wartosciami losowymi
+        /*
+         może przechowywać tylko typy złożone, np. Integer
+         nie musi mieć zdefiniowanego rozmiaru
+         rozmiar może się zmieniać w trakcie
+
+         typy interfejsów:
+         List -> ArrayList, LinkedList
+         Set -> HashSet
+         Map
+        */
+
+        List<Integer> listaLiczbLosowych = new ArrayList<>();
+        //jeśli byłoby na początku ArrayList, nie można by było zmienić
+
+        for (int i = 0; i < 6; i++) {
+            int liczba = (int)(Math.random()*100);
+            listaLiczbLosowych.add(liczba);
+        }
+        System.out.println("Wylosowana lista");
+        for (Integer element:listaLiczbLosowych) {
+            System.out.print(element + ", ");
+        }
+        System.out.println(tablicaLiczbLosowych);
 
     }
 }
