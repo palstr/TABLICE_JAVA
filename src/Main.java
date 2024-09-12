5e235b30bdea43ecc3c1c83f65e0a2b6e299e22f
palstr <palstrgames@gmail.com>
2024-09-12 11:20:33 +0200

    hash set

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Main {
@@ -67,5 +68,21 @@ public class Main {
 
         System.out.println(listaLosowychBezPowtorzen);
 
+        /*
+        lista:
+        indeksowana, uporządkowana, możliwość powtórek
+
+        zbiór: (zazwyczaj)
+        nieindeksowany, nieuporządkowane, elementy unikatowe
+        */
+
+        HashSet<Integer> zbiorLiczbLosowych = new HashSet<>();
+
+        while (zbiorLiczbLosowych.size()<6){
+            liczba = (int)(Math.random()*100+1);
+            zbiorLiczbLosowych.add(liczba);
+        }
+        System.out.println("Zbiór liczb losowych: ");
+        System.out.println(zbiorLiczbLosowych);
     }
 }
