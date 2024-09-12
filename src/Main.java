30da2a226ac2b12c401afefd1b5bada356d0f8fb
palstr <palstrgames@gmail.com>
2024-09-12 10:37:37 +0200

    losowanie do tablicy i wypisywanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        //tablica
+        /*
+         musi mieć zdefiniowany, NIEZMIENNY rozmiar.
+         Przechowuje typy proste i złożone.
+        */
+
+        int [] tablicaLiczbLosowych = new int[6];
+
+        // losujemy wartości 1-100
+
+        for (int i = 0; i < tablicaLiczbLosowych.length; i++) {
+            tablicaLiczbLosowych[i] = (int)(Math.random()*100+1);
+        }
+        //wypisywanie tablicy na ekranie
+
+        for (int element:tablicaLiczbLosowych) {
+            System.out.print(element + ", ");
+        }
+
     }
 }
