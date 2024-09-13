5395b77ce0c589bcb935df15753b62b8281699ff
palstr <palstrgames@gmail.com>
2024-09-13 11:36:53 +0200

    DO KARTKÓWKI

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,34 @@ public class Main {
 
         Gra gra1= new Gra();
         gra1.zagraj();
+        System.out.println("Pierwsze " + sitoEratostelesa(200));
     }
 
+    //DO KARTKÓWKI
+    static ArrayList<Integer> sitoEratostelesa(int liczba){
+        //lista z wartościami logicznymi
+        ArrayList<Boolean> logiczne = new ArrayList<>();
+        logiczne.add(false);
+        logiczne.add(false);
+        for (int i = 2; i < liczba; i++) {
+            logiczne.add(true);
+        }
+        //wykreślanie wielokrotności
+
+        for (int i = 0; i < liczba; i++) {
+            if (logiczne.get(i).equals(true)){
+                //wykreślanie/zmiana na false
+                for (int k = i*2; k<liczba; k=k+i){
+                    logiczne.set(k, false);
+            }
+            }
+        }
+        ArrayList<Integer> pierwsze = new ArrayList<>();
+        for (int i = 0; i <logiczne.size(); i++){
+            if(logiczne.get(i).equals(true)){
+                pierwsze.add(i);
+            }
+        }
+        return pierwsze;
+    }
 }
